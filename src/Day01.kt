85f671a4a994f66544ab812c71f7ce6b4e765c4b
Jakub Sobczak <jakub.sobczak@chess.com>
2024-12-03 20:05:53 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,56 @@
+
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var leftList = mutableListOf<Int>()
+        var rightList = mutableListOf<Int>()
+
+        input.map { it ->
+            it.split(" ").filterNot { it.isBlank() }
+        }.forEach {
+            leftList.add(it[0].toInt())
+            rightList.add(it[1].toInt())
+        }
+
+        leftList.sort()
+        rightList.sort()
+
+        var result = 0
+        leftList.zip(rightList).forEach {
+            result += abs(it.first - it.second)
+        }
+
+        return result
+
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+
+        var leftList = mutableListOf<Int>()
+        var rightMap = mutableMapOf<Int, Int>()
+        input.map { it ->
+            it.split(" ").filterNot { it.isBlank() }
+        }.forEach {
+            leftList.add(it[0].toInt())
+            rightMap.set(it[1].toInt(), rightMap.getOrDefault(it[1].toInt(), 0) + 1)
+        }
+
+        var result = 0
+        leftList.forEach { it ->
+            rightMap.get(it)?.let { mapResult ->
+                result += mapResult * it
+            }
+        }
+            return result
+        }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+//    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
